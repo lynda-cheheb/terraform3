585dcb73053135470057e8c39ab48bfa788df2ec
lynda-cheheb <lynda.cheheb@outlook.fr>
2020-02-28 17:00:56 +0000

    second commit

--- main.tf
+++ main.tf
@@ -76,6 +76,18 @@ resource "azurerm_network_security_group" "myFirstNsg_1" {
    source_address_prefix = "*"
    destination_address_prefix = "*"
   }
+
+  security_rule{
+    name= "jenkins-SSH"
+    priority= "1003"
+    direction= "Inbound"
+    access= "Allow"
+    protocol= "Tcp"
+    source_port_range= "*"
+    destination_port_range= "8080"
+    source_address_prefix= "*"
+    destination_address_prefix= "*"
+  }
 }
 
 resource "azurerm_network_security_group" "myFirstNsg_2" {
@@ -105,6 +117,15 @@ resource "azurerm_network_security_group" "myFirstNsg_2" {
    destination_port_range = "80"
    source_address_prefix = "*"
    destination_address_prefix = "*"
+  }
+  
+  security_rule{
+    name= "jenkins-SSH"
+    priority= "1003"                                                                                              direction= "Inbound"
+    access= "Allow"
+    protocol= "Tcp"                                                                                               source_port_range= "*"
+    destination_port_range= "8080"                                                                                source_address_prefix= "*"
+    destination_address_prefix= "*"
   }
 }
 # create a network interface========================================================================
